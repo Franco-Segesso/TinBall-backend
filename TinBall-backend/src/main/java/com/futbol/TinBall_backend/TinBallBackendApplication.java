357c605fca0760a05400ed99010b58d6ce135b12
Franco-Segesso <fransege@gmail.com>
2026-03-02 14:23:09 -0300

    Initial commit

---
+++ TinBall-backend/src/main/java/com/futbol/TinBall_backend/TinBallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.futbol.TinBall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinBallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinBallBackendApplication.class, args);
+	}
+
+}
---
+++ TinBall-backend/src/test/java/com/futbol/TinBall_backend/TinBallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futbol.TinBall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinBallBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
