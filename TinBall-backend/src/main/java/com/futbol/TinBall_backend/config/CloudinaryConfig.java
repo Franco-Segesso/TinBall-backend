8fb4a4411166ee9f6dd068d8c902bf81f51f5e95
Franco-Segesso <fransege@gmail.com>
2026-03-05 17:18:12 -0300

    cambios para imgs 2

--- TinBall-backend/src/main/java/com/futbol/TinBall_backend/config/CloudinaryConfig.java
+++ TinBall-backend/src/main/java/com/futbol/TinBall_backend/config/CloudinaryConfig.java
@@ -12,8 +12,8 @@ public class CloudinaryConfig {
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
             "cloud_name", "dgvj0kcez",
-            "api_key", "993864933125814",
-            "api_secret", "10IR-gSMqAgy4eTBzwjZKI30lU0"
+            "api_key", "451855175326522",
+            "api_secret", "iXPzGz0A4hfXVUyVO8duIbivERA"
         ));
     }
 }
